566014c191be7c8eb8b5d07864d3ead9acc8a7d4
Rafael Barash <rafabarash@gmail.com>
2019-12-25 11:01:50 -0600

    tab component working (#8)

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,23 +1,28 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { blue, pink, red, yellow, green } from '@material-ui/core/colors';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
 // A custom theme for this app
-const theme = createMuiTheme({
+let theme = createMuiTheme({
   palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
+    primary: blue,
+    secondary: pink,
+    error: red,
+    warning: yellow,
+    success: green,
     background: {
       default: '#e3f2fd',
       // default: '#fff',
     },
   },
+  typography: {
+    fontFamily: ['-apple-system', 'Roboto', '"Helvetica Neue"', 'Arial'].join(
+      ','
+    ),
+    fontSize: 10,
+    htmlFontSize: 10,
+  },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
