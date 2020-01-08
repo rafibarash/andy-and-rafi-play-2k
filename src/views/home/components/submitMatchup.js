4870248eb3e36798bb8683947ffc42d4390b2c2a
Rafael Barash <rafabarash@gmail.com>
2020-01-07 19:42:07 -0600

    quick prettier fix

--- src/views/home/components/submitMatchup.js
+++ src/views/home/components/submitMatchup.js
@@ -88,7 +88,7 @@ const SubmitMatchup = () => {
     const res = await fetch('/.netlify/functions/server/matchup', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(matchup),
     });
